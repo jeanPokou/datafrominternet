a4eca2c957a93f9ab69c8937be9f6f2b9839d462
jeanpokou <pokoujeanmarc@gmail.com>
2017-09-14 23:27:11 -0300

    create Async task

--- app/src/main/java/com/slack/jeanpokou/datafrominternet/MainActivity.kt
+++ app/src/main/java/com/slack/jeanpokou/datafrominternet/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.slack.jeanpokou.datafrominternet
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -7,6 +8,7 @@ import android.view.MenuItem
 import com.slack.jeanpokou.datafrominternet.utilities.NetworkUtils
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +23,13 @@ class MainActivity : AppCompatActivity() {
         return  true
     }
 
+     private fun makeGitHubSearchQuery() {
+        val url  =  NetworkUtils.buildUrl(et_search_box.text.toString())
+        tv_url_display.text =url.toString()
+        tv_github_search_results_json.text = NetworkUtils.getResponseFromHttpUrl(url)
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
 
 //        if(item!!.itemId == R.id.action_search){
@@ -29,16 +38,27 @@ class MainActivity : AppCompatActivity() {
         when(item!!.itemId) {
             R.id.action_search ->{
                 toast("Item  search clicked")
-
+                //      makeGitHubSearchQuery()
                 val url  =  NetworkUtils.buildUrl(et_search_box.text.toString())
-                tv_url_display.text =url.toString()
-                tv_github_search_results_json.text = NetworkUtils.getResponseFromHttpUrl(url)
-
-
+                GitHubQueryTask().execute(url)
             }
 
         }
         return super.onContextItemSelected(item)
+    }
+
+     inner class GitHubQueryTask : AsyncTask<URL, Void, String>() {
+        override fun doInBackground(vararg p0: URL?): String {
+             return NetworkUtils.getResponseFromHttpUrl(p0[0])
+
+        }
+
+        override fun onPostExecute(result: String?) {
+            if(result != null && result != ""){
+                tv_github_search_results_json.text = result
+            }
+        }
+
     }
 }
 
