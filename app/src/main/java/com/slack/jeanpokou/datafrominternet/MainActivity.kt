562b6195225f5ecf140a08d72680baf7b59e5561
jeanpokou <pokoujeanmarc@gmail.com>
2017-09-15 22:16:55 -0300

    Add Polish Coding

--- app/src/main/java/com/slack/jeanpokou/datafrominternet/MainActivity.kt
+++ app/src/main/java/com/slack/jeanpokou/datafrominternet/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import com.slack.jeanpokou.datafrominternet.utilities.NetworkUtils
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.toast
@@ -29,6 +30,17 @@ class MainActivity : AppCompatActivity() {
         tv_github_search_results_json.text = NetworkUtils.getResponseFromHttpUrl(url)
     }
 
+    private fun showErrorMessage() {
+        tv_error_message_display.visibility = View.VISIBLE
+        tv_github_search_results_json.visibility = View.INVISIBLE
+    }
+
+    private fun showJsonDataView() {
+        tv_error_message_display.visibility = View.INVISIBLE
+        tv_github_search_results_json.visibility = View.VISIBLE
+    }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
 
@@ -48,14 +60,23 @@ class MainActivity : AppCompatActivity() {
     }
 
      inner class GitHubQueryTask : AsyncTask<URL, Void, String>() {
+
+         override fun onPreExecute() {
+             pb_loading_indicator.visibility = View.VISIBLE
+             super.onPreExecute()
+         }
         override fun doInBackground(vararg p0: URL?): String {
              return NetworkUtils.getResponseFromHttpUrl(p0[0])
 
         }
 
         override fun onPostExecute(result: String?) {
+            pb_loading_indicator.visibility = View.INVISIBLE
             if(result != null && result != ""){
+                showJsonDataView()
                 tv_github_search_results_json.text = result
+            } else {
+                showErrorMessage()
             }
         }
 
